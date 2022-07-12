0a917c9d138368e2bb1b374b2238a73aca900a01
unknown <carrizocyntia22@gmail.com>
2022-07-12 11:14:36 -0300

    desafio componentesII: contador con boton. Cree un componente ItemCount donde se encuentra los botones para incrementar y disminuir productos al carrito. al finalizar la cantidad deseada con el boton agregar al carrito le puse un alert con la cantidad final del producto a comprar. A dicho componente lo importe al ItemListContainer.

---
+++ src/components/ItemCount.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+const ItemCount = ({initial = 0, stock = 5, onAdd})=>{
+
+    const [rate, setRate] = useState(0);
+
+    useEffect(()=>{
+        setRate(initial);
+    },[])
+
+    const increment = () =>{
+        if (rate < stock){
+            setRate(rate+1);
+        }
+    }
+
+    const decremento = () =>{
+        if(rate <= stock && rate > initial){
+            setRate(rate-1);
+        }
+    }
+
+    return(
+        <>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+             <button class="btn btn-primary me-md-2" type="button" onClick={increment}>+</button>
+             <p>Cantidad: <span>{rate}</span></p>
+             <button class="btn btn-primary" type="button" onClick={decremento}>-</button>
+            
+                 
+                   {
+                    stock
+                    ? <button class="btn btn-primary "  type="button" onClick={()=> onAdd(rate)}>Agregar al Carrito</button>
+                    : <button class="btn btn-primary" type="button" disabled>No hay stock disponible</button>
+             }
+             
+            
+          
+             
+         </div>
+        </>
+
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,19 @@
+import ItemCount from "./ItemCount"
+
 function ItemListContainer(props){
+    
+    const onAdd=(cantidad)=>{
+        alert(`La cantidad de productos seleccionado es: ${cantidad}`)
+    }
     return(
+        <>
         <p>
         {props.greeting}
         </p>
+        <div className="itemCount">
+        <ItemCount  stock={5} initial={0} onAdd={onAdd}></ItemCount>
+        </div>
+        </>
     )
 }
 
