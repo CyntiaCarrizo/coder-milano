3f2feecefe87a8e003e921e52bd8b91ff822c4db
unknown <carrizocyntia22@gmail.com>
2022-08-09 15:31:05 -0300

    desafio:  FirebaseII. Se crea orden de compra en firestore actualizando tamb su cantidad de productos

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,48 @@ import React, { useContext } from 'react';
 import { useCartContext } from './CartContext';
 import { Link } from 'react-router-dom';
 import ItemCart from './ItemCart';
+import { collection, doc, increment, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore';
+import { async } from '@firebase/util';
+import { db } from '../utils/firebaseConf';
+import product from './Products';
 
 const Cart = () => {
-  const {cartList, totalPrice} = useCartContext();
+  const {cartList, totalPrice, clearCart, isInCart} = useCartContext();
+
+  const createOrder=()=>{
+      let order={
+        buyer: {
+          name: "carrizo cyntia",
+          email: "carrizocyntia22@gmail.com",
+          phone: "123123"
+        },
+        date: serverTimestamp(),
+        items: cartList.map(product => ({id: product.id, title: product.nombre, price: product.precio, quantity: product.quantity})),
+        total: totalPrice(),
+
+      }
+      console.log(order)
+
+      const createOrderInFirestore = async()=>{
+        const newOrderRef = doc(collection(db, "orders"))
+        await setDoc(newOrderRef, order)
+        return newOrderRef
+      }
+
+      createOrderInFirestore()
+        .then(result => alert(`Tu orden ha sido creada con exito. Tu numero de pedido es ${result.id}`))
+        .catch(e=> console.log(e))
+
+      cartList.forEach(async(product)=> {
+        const productRef = doc(db, "products", product.id)
+        await updateDoc(productRef, {
+          stock: increment(-product.quantity)
+        })
+      });
+
+       
+  }
+
 
   if (cartList.length === 0){
     return (
@@ -17,10 +56,22 @@ const Cart = () => {
   }
   return (
    <>
+   <Link to={"/"}> <button >Continua Comprando</button></Link>
    {
-   cartList.map(product => <ItemCart key={product.id} product={product}></ItemCart>)
+    cartList.length> 0
+    ? <button onClick={clearCart} >Borrar todos los Productos</button>
+    : <p>Tu carrito esta vacio</p>
+   }
+   
+   {
+    
+   cartList.map(product => <ItemCart key={product.id} product={product}> </ItemCart>)
+                                                        
    }
    <p>Total: {totalPrice()} </p>
+   <button onClick={createOrder}>Orden de compra</button>
+  
+  
    </>
   
   )
